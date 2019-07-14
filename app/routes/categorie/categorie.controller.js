b24e1051ff931e4a88b684d8ce8c891f74863886
Alolu <borniche.leo@gmail.com>
2019-07-14 23:46:38 +0200

    Pagination page categorie

--- app/routes/categorie/categorie.controller.js
+++ app/routes/categorie/categorie.controller.js
@@ -1,22 +1,32 @@
 //@ts-check
-
+const Categorie = require('../../models/categorie.service')
+const categorie = new Categorie()
 /**
  * Main Route Contoller
  * @param {object} router
  */
 module.exports = (router) => {
-    let data = {
-        title: "Oh hi world!",
-    };
-    router.get("/categorie",
+    router.get("/categorie/:id",
         /**
          * @param {object} req
          * @param {object} res
          */
-        (req, res) => {
-            
-            req.vueOptions.head.title = "Categories";
-            res.renderVue("categorie/categorie.vue", data, req.vueOptions);
+        async (req, res) => {
+            let cat = await categorie.getCategorie(req.params.id)
+            let data = {
+
+            }
+            if(cat){
+                req.vueOptions.head.title = "Categorie " + cat.libelle;
+                data = {
+                    categorie: cat,
+                    products: await categorie.getProducts(cat.id)
+                }
+                res.renderVue("categorie/categorie.vue", data, req.vueOptions);
+            }else{
+                req.vueOptions.head.title = "Cette categorie n'existe pas"
+                res.renderVue('error.vue',data,req.vueOptions)  
+            }
         },
     );
 };
