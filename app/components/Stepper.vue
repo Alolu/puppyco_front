3c542cc66f2fbee514ce46cc387e81ac41b76a13
Alolu <borniche.leo@gmail.com>
2019-07-17 17:08:16 +0200

    Start funnel

---
+++ app/components/Stepper.vue
@@ -0,0 +1,120 @@
+<template>
+<div class="container">
+  <div class="row">
+    <div class="col-sm-12 demo">
+      <div class="stepper">
+        <div class="step active">
+          <div class="step-inner">
+            <div class="step-circle"><span>1</span></div>
+          </div>
+        </div>
+        <div class="step active">
+          <div class="step-inner">
+            <div class="step-circle"><span>2</span></div>
+          </div>
+        </div>
+        <div class="step">
+          <div class="step-inner">
+            <div class="step-circle"><span>3</span></div>
+          </div>
+        </div>
+        <div class="step">
+          <div class="step-inner">
+            <div class="step-circle"><span>4</span></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            
+        }
+    }
+}
+</script>
+
+
+<style>
+.stepper {
+  font-size:0.6em;
+  display: flex;
+  justify-content: space-between;
+  align-items: baseline;
+}
+
+.step {
+  display: flex;
+  flex-direction: row;
+  align-items: baseline;
+  width: 100%;
+}
+.step:after {
+  content: '';
+  display: block;
+  width: 100%;
+  position: relative;
+  height: 3px;
+  top: -5px;
+  background-color: #ffcb9b;
+}
+.step:before {
+  content: '';
+  display: block;
+  width: 100%;
+  position: relative;
+  height: 3px;
+  top: -5px;
+  background-color: #ffcb9b;
+}
+.step:last-child {
+  flex-basis: 70%;
+}
+.step:last-child:after {
+  display: none;
+}
+.step:first-child {
+  flex-basis: 70%;
+}
+.step:first-child:before {
+  display: none;
+}
+
+.active {
+  color: white;
+  font-weight: bold;
+}
+.active:before, .active:after {
+  background-color: #FF9A02;
+}
+.active + .step:not(.active):before {
+  background-color: #FF9A02;
+}
+
+.step-inner {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+}
+
+.active .step-circle {
+  background-color: #FF9A02;
+}
+
+.step-circle {
+  height: 30px;
+  width: 30px;
+  border-radius: 20px;
+  background-color: #ffcb9b;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
---
+++ app/routes/funnel/funnel.vue
@@ -0,0 +1,104 @@
+<template>
+<div>
+    <Header></Header>
+    <Hero title="Votre commande">
+        <Stepper></Stepper>
+    </Hero>
+</div>
+</template>
+<script>
+import Header from '../../components/Header.vue';
+import Stepper from '../../components/Stepper.vue';
+import Hero from '../../components/Hero.vue';
+export default {
+    components: {
+        Header,
+        Stepper,
+        Hero
+    },
+    data(){
+        return {
+
+        }
+    }
+}
+</script>
+<style>
+.product {
+    width: 25%;
+    height: 45vh;
+    position:relative;
+    display:inline-block;
+    color: white;
+    cursor: pointer;
+    padding: 0;
+}
+.product__action {
+    position: absolute;
+    bottom: 5%;
+    right: 5%;
+}
+.large {
+    height: 90vh;
+}
+.product__info {
+    position: absolute;
+    opacity: 0;
+    width: 100%;
+    height: 100%;
+    top:0;
+    transition: opacity 0s linear 300ms, opacity 300ms;
+}
+.product__info div {
+    padding: 15px;
+}
+.product__name {
+    display: block;
+}
+.product__desc {
+    display: block;
+}
+.product__price {
+    position: absolute;
+    bottom: 5%;
+}
+.product__img {
+    display: block;
+    width: 100%;
+    height: 100%;
+}
+@media(min-width: 960px){
+    .product__info:hover {
+        background-color: rgba(0, 0, 0, 0.534);
+        opacity: 1;
+        transition: opacity 0s linear 300ms, opacity 300ms;
+    }
+}
+@media(max-width: 960px)
+{  
+    .product__info {
+        opacity: 1;
+    }
+    .large {
+        height: 350px;
+    }
+    .product {
+        width: 50%;
+        height: 350px;
+    }
+
+    .product__desc {
+        display: none;
+    }
+    .product__img {
+        filter:brightness(70%)
+    }
+}
+@media(max-width: 360px){
+    .product {
+        width: 50%;
+        height: 200px;
+    }
+}
+</style>
+
